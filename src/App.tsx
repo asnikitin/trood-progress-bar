c9c9aba2ce031c6f7484f541d9e2f6930383cbd3
drewoz <drewoz@protonmail.com>
2022-10-17 21:48:31 +0200

    show on the App page by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { IItem, ProgressBar } from "./components/ProgressBar";
+
+const mockData = [
+  { name: "Sold", color: "#BD1FBE", value: 677 },
+  { name: "Got free", color: "#FC64FF", value: 23 },
+  { name: "Burned", color: "#00F53D", value: 202 },
+  { name: "Free float", color: "#6D6D6D", value: 323 },
+] as IItem[];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ProgressBar items={mockData} />
       </header>
     </div>
   );
